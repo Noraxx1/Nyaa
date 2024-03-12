c1214df19659c197c66419c03f5d6a7aa75e97c7
Norax <norax_noxx@gmail.com>
2024-03-12 22:30:49 +0100

    update 0.1

--- scripts/fish/not_found.fish
+++ scripts/fish/not_found.fish
@@ -1,6 +1,18 @@
-function fish_command_not_found --on-event fish_command_not_found
-    set cmd $argv[1]
-    echo "Command not found: $cmd"
-    python3 ~/.local/share/nyaa/call_notfound.py $cmd
-end
 
+
+
+# - - - - - - * WARNING * - - - - #
+# you can change the output in co #
+# sole but dont edit the last lin #
+# e it should be the runner one!! #
+# - - - - - - - - - - - - - - - - - 
+
+function not_found --on-event fish_command_not_found
+    set cmd $argv[1] # latest runned command
+    set args $argv[2..-1] # latest command args excluded the first
+
+    echo "Command not found: $cmd $args"
+
+    # runner:
+    python3 -/./local/share/nyaa/hooks/notfound.py $cmd
+end
